957cbc859e0dd422a8c4ee3b19284240d23f7050
Tomasz <tomek_piatek@wp.pl>
2020-10-06 11:43:33 +0200

    make it better

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java -jar /tmp/build_d38f232a/target/pl.pokemon-0.0.1-SNAPSHOT.jar
--- src/main/java/pokemon/pl/pokemon/client/TCGApiClient.java
+++ src/main/java/pokemon/pl/pokemon/client/TCGApiClient.java
@@ -1,5 +1,6 @@
 package pokemon.pl.pokemon.client;
 
+import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
@@ -10,10 +11,7 @@ import pokemon.pl.pokemon.repositories.CardRepo;
 
 import javax.annotation.PostConstruct;
 import javax.persistence.PostUpdate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.Random;
+import java.util.*;
 
 @Component
 public class TCGApiClient {
@@ -36,10 +34,18 @@ public class TCGApiClient {
                 final int iCopy = i;
                 Thread thread = new Thread(() -> {
                     ArrayList<Card> cards = new ArrayList<>();
-                    Cards response = restTemplate.getForObject(URL + "cards?page=" + iCopy + "&pageSize=1000", Cards.class);
+
+                    HttpHeaders headers = new HttpHeaders();
+                    headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+                    headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
+                    HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+
+
+//                    Cards response = restTemplate.getForObject(URL + "cards?page=" + iCopy + "&pageSize=1000", Cards.class);
+                    ResponseEntity<Cards> response = restTemplate.exchange(URL + "cards?page=" + iCopy + "&pageSize=1000", HttpMethod.GET,entity, Cards.class);
                     System.err.println("Downloaded page: " + iCopy);
-                    System.out.println(response.getCards());
-                    cards.addAll(response.getCards());
+                    System.out.println(response.getBody().getCards());
+                    cards.addAll(response.getBody().getCards());
                     cardRepo.saveAll(cards);
                 });
                 thread.start();
